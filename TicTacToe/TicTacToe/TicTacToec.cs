f0ccf6cc3c23c8c06467e2cc1939856942998890
akshata2005 <akshatasawant1805@gmail.com>
2022-02-03 10:47:46 +0530

    [akshata] Adding TicTacToe Board

---
+++ TicTacToe/TicTacToe/TicTacToec.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    public class TicTacToec
+    {
+        public char[] board;
+        public TicTacToec()
+        {
+            board = new char[10];
+        }
+        public void CreateBoard()
+        {
+            TicTacToec game = new TicTacToec();
+            for (int position = 1; position < 10; position++)
+            {
+                game.board[position] = ' ';
+            }
+        }
+    }
+    
+}
